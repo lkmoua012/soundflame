de7fa07e31c2f33f9c4255e411cddbd5a8fd9a47
larry.moua <lkmoua@gmail.com>
2018-11-17 12:39:29 -0800

    redirect changed again

--- server.js
+++ server.js
@@ -123,14 +123,12 @@ app.get('/callback', function (req, res) {
                     console.log(body);
                 });
 
-                res.redirect('https://lit-basin-82430.herokuapp.com/callback');
-
                 // we can also pass the token to the browser to make requests from there
-                /*res.redirect('http://localhost:3000/#' +
+                res.redirect('http://localhost:3000/#' +
                     querystring.stringify({
                         access_token: access_token,
                         refresh_token: refresh_token
-                    }));*/
+                    }));
             } else {
                 res.redirect('/#' +
                     querystring.stringify({
